f55fcffaf38f4b2991ed5c1eb9111ddf9c4d38e7
我梦 <0x4096.peng@gmail.com>
2019-03-09 00:34:47 +0800

    新增身份证工具类

--- src/main/java/com/github/common/utils/encrypt/AesEncryptUtils.java
+++ src/main/java/com/github/common/utils/encrypt/AesEncryptUtils.java
@@ -12,25 +12,26 @@ import javax.crypto.spec.SecretKeySpec;
 /**
  * @author: 0x4096.peng@gmail.com
  * @date: 2018/12/18
- * @instructions: AES加密
+ * @instructions: AES加密,对称加密
  */
 public class AesEncryptUtils {
 
-
-    private static final Logger logger = LoggerFactory.getLogger(AesEncryptUtils.class);
 
     /**
      * 加密 KEY 必须为 16 位
      */
     private static final String KEY = "1024204840968192";
+
     /**
-     * charSet字符集编码
+     * 默认字符集编码
      */
-    private static final String charsetName = "utf-8";
+    private static final String DEFAULT_ENCODING = "utf-8";
+
     /**
-     * 签名算法
+     * 填充方式
      */
-    public static final String SIGN_ALGORITHMS = "AES/ECB/PKCS5Padding";
+    public static final String PADDING = "AES/ECB/PKCS5Padding";
+
     /**
      * 加密算法
      */
@@ -43,23 +44,23 @@ public class AesEncryptUtils {
      * @param encryptKey
      * @return
      */
-    public static String aesEncrypt(String content,String encryptKey) {
+    public static String encrypt(String content,String encryptKey) {
         if (content == null || encryptKey == null) {
             throw new IllegalArgumentException("加密内容或加密key不能为null");
         }
         if( encryptKey.length() != 16 ){
-            throw new IllegalArgumentException("解密的encryptKey必须为16位");
+            throw new IllegalArgumentException("加密的encryptKey必须为16位");
         }
 
         try {
-            Cipher cipher = Cipher.getInstance(SIGN_ALGORITHMS);
-            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(encryptKey.getBytes(charsetName), KEY_ALGORITHM));
-            byte[] bytes = cipher.doFinal(content.getBytes(charsetName));
+            Cipher cipher = Cipher.getInstance(PADDING);
+            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(encryptKey.getBytes(DEFAULT_ENCODING), KEY_ALGORITHM));
+            byte[] bytes = cipher.doFinal(content.getBytes(DEFAULT_ENCODING));
             String result = Base64.encodeBase64String(bytes);
             /* 解决Base64加密换行问题 */
             return result;
         } catch (Exception e) {
-            logger.error("加密失败:{}",content,e);
+            e.printStackTrace();
         }
         return null;
     }
@@ -69,8 +70,8 @@ public class AesEncryptUtils {
      * @param content
      * @return
      */
-    public static String aesEncrypt(String content) {
-        return aesEncrypt(content,KEY);
+    public static String encrypt(String content) {
+        return encrypt(content,KEY);
     }
 
     /**
@@ -79,22 +80,22 @@ public class AesEncryptUtils {
      * @param decryptKey
      * @return
      */
-    public static String aesDecrypt(String content,String decryptKey) {
+    public static String decrypt(String content,String decryptKey) {
         if (content == null || decryptKey == null) {
-            throw new IllegalArgumentException("加密内容或加密key不能为null");
+            throw new IllegalArgumentException("解密内容或解密key不能为null");
         }
         if( decryptKey.length() != 16 ){
             throw new IllegalArgumentException("解密的decryptKey必须为16位");
         }
 
         try{
-            Cipher cipher = Cipher.getInstance(SIGN_ALGORITHMS);
-            cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(decryptKey.getBytes(charsetName), KEY_ALGORITHM));
+            Cipher cipher = Cipher.getInstance(PADDING);
+            cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(decryptKey.getBytes(DEFAULT_ENCODING), KEY_ALGORITHM));
             byte[] bytes = new BASE64Decoder().decodeBuffer(content);
             bytes = cipher.doFinal(bytes);
-            return new String(bytes, charsetName);
+            return new String(bytes, DEFAULT_ENCODING);
         }catch (Exception e){
-            logger.error("解密失败:{}",content,e);
+            e.printStackTrace();
         }
         return null;
     }
@@ -104,8 +105,8 @@ public class AesEncryptUtils {
      * @param content
      * @return
      */
-    public static String aesDecrypt(String content) {
-        return aesDecrypt(content,KEY);
+    public static String decrypt(String content) {
+        return decrypt(content,KEY);
     }
 
 }

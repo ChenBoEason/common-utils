46a672dfa847854a5936bfe6e2cc6119f7c3c846
0x4096 <0x4096.peng@gmail.com>
2020-05-17 00:14:26 +0800

    update: 新增snakeyaml&调整部分类

---
+++ src/main/java/com/github/x4096/common/utils/properties/YmlUtils.java
@@ -0,0 +1,11 @@
+package com.github.x4096.common.utils.properties;
+
+/**
+ * @author 0x4096.peng@gmail.com
+ * @project common-utils
+ * @datetime 2020/5/16 22:22
+ * @description
+ * @readme
+ */
+public class YmlUtils {
+}
